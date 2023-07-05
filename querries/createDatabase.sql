d0ff2f6ae8deabce5bd8c4e63da8635764bf6120
Tram Nguyen <tramngoc1402@gmail.com>
2023-07-05 23:04:19 +0300

    finished retrieval query

--- querries/createDatabase.sql
+++ querries/createDatabase.sql
@@ -1,96 +1,104 @@
 -- Create database by superuser
-\c - tramnguyen
-CREATE DATABASE company_database;
+
+\c - tramnguyen create database company_database;
 
 -- Create table titles
-CREATE TABLE public.titles
-(
-    id serial,
-    name character varying(30) NOT NULL,
-    PRIMARY KEY (id)
-);
 
-ALTER TABLE IF EXISTS public.titles
-    OWNER to admin;
+create table
+    public.titles (
+        id serial,
+        name character varying(30) not null,
+        primary key (id)
+    );
+
+alter table if exists public.titles owner to admin;
 
 -- Import file to table titles
-psql -U admin -d company_database -c "\copy public.titles(name) FROM 'C:\Users\tramn\OneDrive\Desktop\code\integrify\backend\fs15_16_company-database\data\titles.csv' DELIMITER ',' CSV HEADER"
+
+psql - U admin - d company_database - c "\copy public.titles(name) FROM 'C:\Users\tramn\OneDrive\Desktop\code\integrify\backend\fs15_16_company-database\data\titles.csv' DELIMITER ',' CSV HEADER";
 
 -- Create table teams
-CREATE TABLE public.teams
-(
-    id serial,
-    team_name character varying(30) NOT NULL,
-    location character varying(30) NOT NULL,
-    PRIMARY KEY (id)
-);
+
+create table
+    public.teams (
+        id serial,
+        team_name character varying(30) not null,
+        location character varying(30) not null,
+        primary key (id)
+    );
 
 -- Import file to table teams
-ALTER TABLE IF EXISTS public.teams
-    OWNER to admin;
 
-psql -U admin -d company_database -c "\copy public.teams(team_name, location) FROM 'C:\Users\tramn\OneDrive\Desktop\code\integrify\backend\fs15_16_company-database\data\teams.csv' DELIMITER ',' CSV HEADER"
+alter table if exists public.teams owner to admin;
+
+psql - U admin - d company_database - c "\copy public.teams(team_name, location) FROM 'C:\Users\tramn\OneDrive\Desktop\code\integrify\backend\fs15_16_company-database\data\teams.csv' DELIMITER ',' CSV HEADER";
 
 -- Create table projects
-CREATE TABLE public.projects
-(
-    id serial,
-    name character varying(50) NOT NULL,
-    client character varying(50) NOT NULL,
-    start_date date NOT NULL,
-    deadline date NOT NULL,
-    PRIMARY KEY (id)
-);
 
-ALTER TABLE IF EXISTS public.projects
-    OWNER to admin;
+create table
+    public.projects (
+        id serial,
+        name character varying(50) not null,
+        client character varying(50) not null,
+        start_date date not null,
+        deadline date not null,
+        primary key (id)
+    );
+
+alter table if exists public.projects owner to admin;
 
 -- Import file to table projects
-psql -U admin -d company_database -c "\copy public.projects(name,client,start_date,deadline) FROM 'C:\Users\tramn\OneDrive\Desktop\code\integrify\backend\fs15_16_company-database\data\projects.csv' DELIMITER ',' CSV HEADER"
+
+psql - U admin - d company_database - c "\copy public.projects(name,client,start_date,deadline) FROM 'C:\Users\tramn\OneDrive\Desktop\code\integrify\backend\fs15_16_company-database\data\projects.csv' DELIMITER ',' CSV HEADER";
 
 -- Create table team_project
-create table if not exists public.team_project(
-	team_id int references public.teams (id) on delete cascade on update cascade,
-	project_id int references public.projects (id) on delete cascade on update cascade,
-    PRIMARY KEY(team_id, project_id)
-);
 
-ALTER TABLE IF EXISTS public.team_project
-    OWNER TO admin;
+create table
+    if not exists public.team_project (
+        team_id int references public.teams (id) on delete cascade on update cascade,
+        project_id int references public.projects (id) on delete cascade on update cascade,
+        primary key(team_id, project_id)
+    );
+
+alter table if exists public.team_project owner to admin;
 
 -- Import file to table team_project
-psql -U admin -d company_database -c "\copy public.team_project(team_id, project_id) FROM 'C:\Users\tramn\OneDrive\Desktop\code\integrify\backend\fs15_16_company-database\data\team_project.csv' DELIMITER ',' CSV HEADER"
+
+psql - U admin - d company_database - c "\copy public.team_project(team_id, project_id) FROM 'C:\Users\tramn\OneDrive\Desktop\code\integrify\backend\fs15_16_company-database\data\team_project.csv' DELIMITER ',' CSV HEADER";
 
 -- Create table employees
-create table if not exists public.employees(
-	id serial primary key,
-	first_name varchar(30) not null,
-	last_name varchar(30) not null,
-	hire_date date not null,
-	hourly_salary numeric(10,2) not null,
-	title_id int references public.titles (id) on delete cascade on update cascade,
-	manager_id int references public.employees(id) on delete cascade on update cascade,
-	team_id int references public.teams (id) on delete cascade on update cascade
-);
 
-ALTER TABLE IF EXISTS public.employees
-    OWNER TO admin;
+create table
+    if not exists public.employees (
+        id serial primary key,
+        first_name varchar(30) not null,
+        last_name varchar(30) not null,
+        hire_date date not null,
+        hourly_salary numeric(10, 2) not null,
+        title_id int references public.titles (id) on delete cascade on update cascade,
+        manager_id int references public.employees(id) on delete cascade on update cascade,
+        team_id int references public.teams (id) on delete cascade on update cascade
+    );
+
+alter table if exists public.employees owner to admin;
 
 -- Import file to table employees
-psql -U admin -d company_database -c "\copy public.employees(first_name, last_name, hire_date, hourly_salary, title_id, manager_id, team_id) FROM 'C:\Users\tramn\OneDrive\Desktop\code\integrify\backend\fs15_16_company-database\data\employees.csv' DELIMITER ',' CSV HEADER NULL 'NULL'"
+
+psql - U admin - d company_database - c "\copy public.employees(first_name, last_name, hire_date, hourly_salary, title_id, manager_id, team_id) FROM 'C:\Users\tramn\OneDrive\Desktop\code\integrify\backend\fs15_16_company-database\data\employees.csv' DELIMITER ',' CSV HEADER NULL 'NULL'";
 
 -- Create table hour_tracking
-create table if not exists public.hour_tracking(
-	employee_id int references public.employees (id) on delete cascade on update cascade,
-	project_id int references public.projects (id) on delete cascade on update cascade,
-	total_hours int not null,
-	PRIMARY KEY(employee_id, project_id),
-	check(total_hours > 0)
-	
-);
 
-ALTER TABLE IF EXISTS public.hour_tracking
-    OWNER TO admin;
+create table
+    if not exists public.hour_tracking (
+        employee_id int references public.employees (id) on delete cascade on update cascade,
+        project_id int references public.projects (id) on delete cascade on update cascade,
+        total_hours int not null,
+        primary key(employee_id, project_id),
+        check(total_hours > 0)
+    );
+
+alter table if exists public.hour_tracking owner to admin;
 
 -- Import file to table hour_tracking
-psql -U admin -d company_database -c "\copy public.hour_tracking(employee_id, project_id, total_hours) FROM 'C:\Users\tramn\OneDrive\Desktop\code\integrify\backend\fs15_16_company-database\data\hour_tracking.csv' DELIMITER ',' CSV HEADER"
+
+psql - U admin - d company_database - c "\copy public.hour_tracking(employee_id, project_id, total_hours) FROM 'C:\Users\tramn\OneDrive\Desktop\code\integrify\backend\fs15_16_company-database\data\hour_tracking.csv' DELIMITER ',' CSV HEADER"
--- querries/retrieval.sql
+++ querries/retrieval.sql
@@ -1,9 +1,79 @@
 -- - Retrieve the team names and their corresponding project count.
+
 select
     teams.team_name,
     count(team_project.project_id) as project_count
 from teams
-    right join team_project on teams.id = team_project.team_id
+    left join team_project on teams.id = team_project.team_id
 group by
     teams.id,
     teams.team_name;
+
+-- Retrieve all the employees (both directly and indirectly) working under Andrew Martin
+
+select e2.*
+from employees e1
+    join employees e2 ON e1.id = e2.manager_id
+where
+    e1.first_name = 'Andrew'
+    and e1.last_name = 'Martin';
+
+-- Retrieve all the employees (both directly and indirectly) working under Robert Brown
+
+select e2.*
+from employees e1
+    join employees e2 ON e1.id = e2.manager_id
+where
+    e1.first_name = 'Robert'
+    and e1.last_name = 'Brown';
+
+-- Retrieve the average hourly salary for each title.
+
+select
+    titles.name,
+    avg(hourly_salary) as average_hourly_salary
+from titles
+    join employees ON titles.id = employees.title_id
+group by
+    titles.id,
+    titles.name
+order by avg(hourly_salary);
+
+-- Retrieve the employees who have a higher hourly salary than their respective team's average hourly salary.
+
+select
+    e.*,
+    team_avg.team_avg_salary
+from employees e
+    join (
+        select
+            team_id,
+            AVG(hourly_salary) AS team_avg_salary
+        from employees
+        group by
+            team_id
+    ) as team_avg on e.team_id = team_avg.team_id
+where
+    e.hourly_salary > team_avg.team_avg_salary
+order by team_id;
+
+-- Retrieve the projects that have more than 3 teams assigned to them.
+
+select
+    projects.*,
+    count(team_project.team_id) as number_of_assigned_teams
+from projects
+    join team_project on projects.id = team_project.project_id
+group by projects.id
+having
+    count(team_project.team_id) > 3;
+
+-- Retrieve the total hourly salary expense for each team.
+
+select
+    team_id,
+    sum(hourly_salary) as total_hourly_salary_expense
+from employees
+where team_id is not null
+group by team_id
+order by team_id;
