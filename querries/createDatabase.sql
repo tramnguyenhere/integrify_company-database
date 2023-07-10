ebcbb1d1782af6865fc129db4670d45a749614e9
Tram Nguyen <tramngoc1402@gmail.com>
2023-07-10 14:17:02 +0300

    created columns created_at and updated_at, wrote trigger function for updating updated_at

--- querries/createDatabase.sql
+++ querries/createDatabase.sql
@@ -12,6 +12,8 @@ create table
     public.titles (
         id serial,
         name character varying(30) not null,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         primary key (id)
     );
 
@@ -28,6 +30,8 @@ create table
         id serial,
         team_name character varying(30) not null,
         location character varying(30) not null,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         primary key (id)
     );
 
@@ -46,6 +50,8 @@ create table
         client character varying(50) not null,
         start_date date not null,
         deadline date not null,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         primary key (id)
     );
 
@@ -61,6 +67,8 @@ create table
     if not exists public.team_project (
         team_id int references public.teams (id) on delete cascade on update cascade,
         project_id int references public.projects (id) on delete cascade on update cascade,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         primary key(team_id, project_id)
     );
 
@@ -81,7 +89,9 @@ create table
         hourly_salary numeric(10, 2) not null,
         title_id int references public.titles (id) on delete cascade on update cascade,
         manager_id int references public.employees(id) on delete cascade on update cascade,
-        team_id int references public.teams (id) on delete cascade on update cascade
+        team_id int references public.teams (id) on delete cascade on update cascade,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     );
 
 alter table if exists public.employees owner to admin;
@@ -97,6 +107,8 @@ create table
         employee_id int references public.employees (id) on delete cascade on update cascade,
         project_id int references public.projects (id) on delete cascade on update cascade,
         total_hours int not null,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         primary key(employee_id, project_id),
         check(total_hours > 0)
     );
@@ -105,4 +117,63 @@ alter table if exists public.hour_tracking owner to admin;
 
 -- Import file to table hour_tracking
 
-psql - U admin - d company_database - c "\copy public.hour_tracking(employee_id, project_id, total_hours) FROM 'C:\Users\tramn\OneDrive\Desktop\code\integrify\backend\fs15_16_company-database\data\hour_tracking.csv' DELIMITER ',' CSV HEADER"
+psql - U admin - d company_database - c "\copy public.hour_tracking(employee_id, project_id, total_hours) FROM 'C:\Users\tramn\OneDrive\Desktop\code\integrify\backend\fs15_16_company-database\data\hour_tracking.csv' DELIMITER ',' CSV HEADER";
+
+-- Add trigger update for update_at in each entity
+
+CREATE OR REPLACE FUNCTION UPDATE_UPDATED_AT() RETURNS 
+TRIGGER AS 
+	$$ BEGIN NEW.updated_at = CURRENT_TIMESTAMP;
+	RETURN 
+NEW; 
+
+END;
+
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER UPDATE_TITLES_UPDATED_AT 
+	BEFORE
+	UPDATE
+	    ON public.titles FOR EACH ROW
+	EXECUTE
+	    FUNCTION update_updated_at();
+	--
+	CREATE TRIGGER UPDATE_TEAMS_UPDATED_AT 
+		BEFORE
+		UPDATE
+		    ON public.teams FOR EACH ROW
+		EXECUTE
+		    FUNCTION update_updated_at();
+		--
+		CREATE TRIGGER UPDATE_TEAM_PROJECT_UPDATED_AT 
+			BEFORE
+			UPDATE
+			    ON public.team_project FOR EACH ROW
+			EXECUTE
+			    FUNCTION update_updated_at();
+			--
+			CREATE TRIGGER UPDATE_EMPLOYEES_UPDATED_AT 
+				BEFORE
+				UPDATE
+				    ON public.employees FOR EACH ROW
+				EXECUTE
+				    FUNCTION update_updated_at();
+				--
+				CREATE TRIGGER UPDATE_HOUR_TRACKING_UPDATED_AT 
+					BEFORE
+					UPDATE
+					    ON public.hour_tracking FOR EACH ROW
+					EXECUTE
+					    FUNCTION update_updated_at();
+					--
+					CREATE TRIGGER UPDATE_PROJECTS_UPDATED_AT 
+						BEFORE
+						UPDATE
+						    ON public.projects FOR EACH ROW
+						EXECUTE
+						    FUNCTION update_updated_at();
+					
+				
+			
+		
+	
